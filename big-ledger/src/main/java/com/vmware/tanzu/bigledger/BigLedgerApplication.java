009dd88ab992b19439d71f1909834abdf1cdc143
jose <49160281+jmartRW@users.noreply.github.com>
2022-05-06 11:46:54 -0400

    Initial commit.

---
+++ big-ledger/src/main/java/com/vmware/tanzu/bigledger/BigLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.vmware.tanzu.bigledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigLedgerApplication.class, args);
+	}
+
+}
---
+++ big-ledger/src/test/java/com/vmware/tanzu/bigledger/BigLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmware.tanzu.bigledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
